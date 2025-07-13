c4d572f8746c02eceda893ff48350f00f087c29c
Balam Alcaraz <balamalcaraz@MacBook-Air-de-Balam.local>
2025-07-12 23:44:43 -0600

    Create| medico-paciente  controller, entities, records, repository and DB migrations

---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,37 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.direccion.Direccion;
+
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String telefono;
+    private String documento_identidad;
+
+    @Embedded
+    private Direccion direccion;
+
+    public Paciente(DatosRegistroPaciente datos) {
+        this.nombre = datos.nombre();
+        this.email = datos.email();
+        this.telefono = datos.telefono();
+        this.documento_identidad = datos.documento_identidad();
+        this.direccion = new Direccion(datos.datosDireccion());
+    }
+}
